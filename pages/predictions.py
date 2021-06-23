b8ab78b52139fcbc7b7d8eee68cd68bddbfe446e
mramputatoes <mramputatoes@gmail.com>
2021-06-23 15:29:11 -0600

    Updated and cleaned a few things.

--- pages/predictions.py
+++ pages/predictions.py
@@ -6,46 +6,19 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
-
+from joblib import load
 
 # Imports from this application
 from app import app
-from joblib import load
-
 
 model = load('assets/firstmodel.joblib')
-
-@app.callback(
-    Output('prediction-values', 'children'),
-    [Input('host_response_time', 'value'),
-        Input('host_is_superhost', 'value'),
-        Input('host_identity_verified', 'value'),
-        Input('property_type', 'value'),
-        Input('room_type', 'value'),
-        Input('accommodates', 'value'),
-        Input('bedrooms', 'value'),
-        Input('beds', 'value')]
-)
-
-def predict(host_response_time, host_is_superhost,
-    host_identity_verified, property_type, room_type,
-    accommodates, bedrooms, beds):
-    df = pd.DataFrame(
-        columns = ['host_response_time', 'host_is_superhost',
-                'host_identity_verified', 'property_type', 'room_type',
-                'accommodates', 'bedrooms', 'beds'],
-        data = [[host_response_time, host_is_superhost,
-                host_identity_verified, property_type, room_type,
-                accommodates, bedrooms, beds]]),
-    y_pred = model.predict(df)[0]
-    return f"You're rental price:, ${y_pred:.0f}"
-
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
-        #dbc.Label("Host's Response Time"),
+        
+        
         dcc.Markdown("### Response Time"),
         dcc.Dropdown(
             id='host_response_time',
@@ -58,26 +31,63 @@ column1 = dbc.Col(
             value='within an hour',
             className='mb-5',
         ),
-        #dbc.Label('Host is superhost?'),
         dcc.Markdown("### Superhost?"),
         dcc.Dropdown(
             id='host_is_superhost',
-            options = [{'label': 'Yes', 'value': 1},
-                {'label': 'No', 'value': 0}],
-            value = 'No',
-            className = 'mb-5',
+            options = [{'label': 'Yes', 'value': '1.0'},
+                {'label': 'No', 'value': '0.0'}],
+            value='0.0',
+            className = 'mb-5'
         ),
-        #dbc.Label('Host identity verified?'),
         dcc.Markdown("### Verified Identity?"),
         dcc.Dropdown(
                 id = 'host_identity_verified',
-                options=[{'label': 'Yes', 'value': 1},
-                    {'label': 'No', 'value': 0}],
-                value='No',
+                options=[{'label': 'Yes', 'value': '1.0'},
+                    {'label': 'No', 'value': '0.0'}],
+                value='0.0',
                 className='mb-5',
         ),
-        #dbc.Label('Property Type'),
-        dcc.Markdown('#### Property Type'),
+
+        dcc.Markdown('#### Number of Beds'),
+        dcc.Slider(id='beds',
+                   min=1,
+                   max=50,
+                   step=1,
+                   value=0,
+                   marks={n: str(n) for n in range(1, 54, 2)}
+                   ),
+        dcc.Markdown('', id='output_beds',
+                     style={'textAlign': 'center',
+                            'font-size': 18},
+                     className='mb-5'),
+
+        dcc.Markdown('#### Number of Bedrooms'),
+        dcc.Slider(id='bedrooms',
+                   min=1,
+                   max=50,
+                   step=1,
+                   value=0,
+                   marks={n: str(n) for n in range(1, 54, 2)}
+                   ),
+        dcc.Markdown('', id='output_bedrooms',
+                    style={'textAlign': 'center',
+                        'font-size': 18},
+                    className='mb-5'),                  
+
+        dcc.Markdown('#### Accommodates'),
+        dcc.Slider(id='accommodates',
+                   min=2,
+                   max=16,
+                   step=1,
+                   value=1,
+                   marks={n: str(n) for n in range(2, 17, 1)}
+                   ),
+        dcc.Markdown('', id='output_accommodates',
+                    style={'textAlign': 'center',
+                        'font-size': 18},
+                    className='mb-5'),
+
+        dcc.Markdown('#### Property type'),
         dcc.Dropdown(
             id='property_type',
             options=[
@@ -129,67 +139,87 @@ column1 = dbc.Col(
                 {"label": "Private room in villa","value": "Private room in villa"},
                 {"label": "Private room in cabin","value": "Private room in cabin"},
                 {"label": "Entire place ","value": "Entire place"}
-                ],
-            value='Campsite',
-            className='mb-5',
+
+            ],
+            value='Entire apartment', className='mb-5'
         ),
-        #dbc.Label('Room Type'),
+
         dcc.Markdown('#### Room Type'),
         dcc.Dropdown(
             id='room_type',
             options=[
-                {"label": "Entire home/apt", "value": "Entire home/apt"},
-                {"label": "Private room", "value": "Private room"},
-                {"label": "Shared room","value": "Shared room"},
-                {"label": "Hotel room","value": "Hotel room"}
-                    ],
-            value="Entire home/apt",
-            className='mb-5',
-        ),
-        #dbc.Label('Accommodates'),
-        dcc.Markdown('#### Accommodates'),
-        dcc.Slider(
-            id='accommodates',
-            min=2,
-            max=16,
-            step=1,
-            value=1,
-            marks={n: str(n) for n in range(2,17,1)},
-            className='mb-5',
-        ),
-        #dbc.Label('Bedrooms'),
-        dcc.Markdown('#### Bedrooms'),
-        dcc.Slider(
-            id='bedrooms',
-            min=1,
-            max=50,
-            step=1,
-            value=1,
-            marks={n: str(n) for n in range(1,51,1)},
-            className='mb-5',
-        ),
-        #dbc.Label('Beds'),
-        dcc.Markdown('#### Beds'),
-        dcc.Slider(
-            id='beds',
-            min=1,
-            max=50,
-            step=1,
-            value=1,
-            marks={n: str(n) for n in range(1,51,1)},
-            className='mb-5',
-        )      
+                {'label': 'Entire home/apt', 'value': 'Entire home/apt'},
+                {'label': 'Private room', 'value': 'Private room'},
+                {'label': 'Shared room', 'value': 'Shared room'},
+                {'label': 'Hotel room', 'value': 'Hotel room'}
+            ],
+            value='Entire home/apt', className='mb-5'
+        )
+
     ],
-    md=4,
+    md=6,
 )
 
 column2 = dbc.Col(
     [
-        dcc.Markdown('### RENT THIS MUCH!!!!'),
-        html.H2('Test'),
-        html.Div(id='prediction-values')
-    ],
-    md=4
+        dcc.Markdown('### Predicted Rental price ($)'),
+        daq.LEDDisplay(
+            id='prediction-content',
+            size=25,
+            color="#42f55d"),
+        html.Img(src='assets/airbnb_pic7.jpg', className='img-fluid')
+    ]
 )
 
-layout = dbc.Row([column1,column2])
+
+@app.callback(
+    Output('prediction-content', 'value'),
+    [Input('host_response_time', 'value'),
+        Input('host_is_superhost', 'value'),
+        Input('host_identity_verified', 'value'),
+        Input('property_type', 'value'),
+        Input('room_type', 'value'),
+        Input('accommodates', 'value'),
+        Input('bedrooms', 'value'),
+        Input('beds', 'value')])
+
+def predict(host_response_time, host_is_superhost,
+    host_identity_verified, property_type, room_type,
+    accommodates, bedrooms, beds):
+    df = pd.DataFrame(
+        columns=['host_response_time', 'host_is_superhost',
+                'host_identity_verified', 'property_type', 'room_type',
+                'accommodates', 'bedrooms', 'beds'],
+        data=[
+            [host_response_time, host_is_superhost,
+                host_identity_verified, property_type, room_type,
+                accommodates, bedrooms, beds]])
+    y_pred = model.predict(df)[0]
+    y_round = round(y_pred, 2)
+    return y_round
+
+@app.callback(
+    Output('output_beds', 'children'),
+    [Input('beds', 'value')])
+def update_beds(input_value):
+    return '{} beds'.format(input_value)
+
+
+@app.callback(
+    Output('output_bedrooms', 'children'),
+    [Input('bedrooms', 'value')])
+def update_beds(input_value):
+    return '{} bedrooms'.format(input_value)
+
+
+
+@app.callback(
+    Output('output_accommodates', 'children'),
+    [Input('accommodates', 'value')])
+def update_beds(input_value):
+    return '{} accommodates'.format(input_value)
+
+
+
+
+layout = dbc.Row([column1, column2])
